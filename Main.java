93633e6f4ecfc70b6f12ef20ff7b1de476fe113d
Nikoloz <57254525+Nick-Geo@users.noreply.github.com>
2019-12-10 18:20:00 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import javafx.scene.Node;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        class LinkedList {
+            Node head;
+
+            class Node {
+                int data;
+                Node next;
+
+                Node(int d) {
+                    data = d;
+                    next = null;
+                } // Constructor
+            }
+
+            public void main(String[] args) {
+
+                LinkedList llist = new LinkedList();
+
+                llist.head = new Node(1);
+                Node second = new Node(2);
+                Node third = new Node(3);
+
+                llist.head.next = second;
+                second.next = third;
+            }
+        }
+    }
+}
